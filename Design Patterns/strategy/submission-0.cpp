69b54b6b8a15dc3cd9196fdfe71eed8d179885eb
Zach <62490702+Zach-Harding@users.noreply.github.com>
2026-04-28 14:02:29 -0500

    Add: strategy - submission-0

---
+++ Design Patterns/strategy/submission-0.cpp
@@ -0,0 +1,74 @@
+class Person {
+private:
+    string lastName;
+    int age;
+    bool married;
+
+public:
+    Person(string lastName, int age, bool married) 
+        : lastName(lastName), age(age), married(married) {}
+
+    string getLastName() {
+        return lastName;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    bool isMarried() {
+        return married;
+    }
+};
+
+class PersonFilter {
+public:
+    virtual ~PersonFilter() {}
+    virtual bool apply(Person& person) = 0;
+};
+
+class AdultFilter : public PersonFilter {
+    // Implement Adult filter
+    bool apply(Person& person) override {
+        if(person.getAge() >= 18) return true;
+        return false;
+    }
+
+};
+
+class SeniorFilter : public PersonFilter {
+    // Implement Senior filter
+    bool apply(Person& person) override {
+        if(person.getAge() >= 65) return true;
+        return false;
+    }
+};
+
+class MarriedFilter : public PersonFilter {
+    // Implement Married filter
+    bool apply(Person& person) override {
+        return person.isMarried();
+    }
+    
+};
+
+class PeopleCounter {
+private:
+    PersonFilter* filter;
+
+public:
+    void setFilter(PersonFilter& filter) {
+        this->filter = &filter;
+    }
+
+    int count(vector<Person>& people) {
+        // Implement method here
+        int count = 0;
+        for(auto& person: people) {
+            if (this->filter->apply(person)) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
